639b2b935b5ac4a135df22ca89293db3eaa17a47
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-04-26 17:12:09 +0200

    write tests

---
+++ 01-complete-intro-to-react-v9/02-tools/src/__test__/Pizza.test.jsx
@@ -0,0 +1,25 @@
+import { render, cleanup } from "@testing-library/react";
+import { expect, test, afterEach } from "vitest";
+import Pizza from "../Pizza";
+
+afterEach(cleanup);
+
+test("alt test renders on Pizza image", async () => {
+  const name = "My favorite pizza";
+  const src = "https://picsum.photos/200";
+  const screen = render(
+    <Pizza name={name} description="super cool pizza" image={src} />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
+
+test("alt test renders on Pizza image", async () => {
+  const name = "My favorite pizza";
+  const screen = render(<Pizza name={name} />);
+
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe("");
+});
