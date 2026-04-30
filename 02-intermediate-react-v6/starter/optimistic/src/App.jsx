c1d2b8cf31c36c8a160beb0d9d2074df09c42718
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-04-30 20:57:25 +0200

    Add optimistic deep thoughts form demo

--- 02-intermediate-react-v6/starter/optimistic/src/App.jsx
+++ 02-intermediate-react-v6/starter/optimistic/src/App.jsx
@@ -1,3 +1,69 @@
+import { useEffect, useOptimistic, useState, useTransition } from "react";
+
 export default function App() {
-  return <h1>Deep Thoughts</h1>;
+  const [isPending, startTransition] = useTransition();
+  const [thoughts, setThoughts] = useState([]);
+  const [thought, setThought] = useState("");
+  const [optimisticThoughts, addOptimisticThoughts] = useOptimistic(
+    thoughts,
+    (oldThoughts, newThought) => [newThought, ...oldThoughts],
+  );
+
+  async function postDeepThought() {
+    startTransition(async () => {
+      addOptimisticThoughts(`${thought} (Loading...)`);
+      setThought("");
+      const response = await fetch("/thoughts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ thought }),
+      });
+
+      if (!response.ok) {
+        alert("This thought was not deep enough. Do better");
+        return;
+      }
+
+      const { thoughts: newThoughts } = await response.json();
+      setThoughts(newThoughts);
+    });
+  }
+
+  useEffect(() => {
+    fetch("/thoughts")
+      .then((res) => res.json())
+      .then((data) => {
+        setThoughts(data);
+      });
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Deep Thoughts</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          postDeepThought();
+        }}
+      >
+        <label htmlFor="thought">What's on your mind?</label>
+        <textarea
+          id="thought"
+          name="thought"
+          rows="5"
+          cols="33"
+          value={thought}
+          onChange={(e) => setThought(e.target.value)}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {optimisticThoughts.map((thought, index) => (
+          <li key={index}>{thought}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
