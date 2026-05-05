38d90bcb070a5c47a05bc9f5b6b817ff81d3827d
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-05-05 21:03:48 +0200

    complete name tag and nested boxes challenges

--- 04-reaact-and-typescript/src/examples/typing-children/nested-boxes.tsx
+++ 04-reaact-and-typescript/src/examples/typing-children/nested-boxes.tsx
@@ -8,10 +8,11 @@
  * React.ReactChild[];
  */
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type BoxProps = { children: any }; /* 👈 Get rid of this! 👆 */
+import type { PropsWithChildren } from 'react';
 
-const Box = ({ children }: BoxProps) => {
+// type BoxProps = { children: ReactNode };
+
+const Box = ({ children }: PropsWithChildren) => {
   return (
     <section className="m-4" style={{ padding: '1em', border: '5px solid purple' }}>
       {children}
