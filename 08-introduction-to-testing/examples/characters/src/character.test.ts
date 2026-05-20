687ed971ecd8157892e06dba2acb493fd6143aac
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-05-20 18:59:44 +0200

    add tests

--- 08-introduction-to-testing/examples/characters/src/character.test.ts
+++ 08-introduction-to-testing/examples/characters/src/character.test.ts
@@ -1,14 +1,53 @@
-import { describe, it, expect } from 'vitest';
+import { beforeEach, describe, it, expect, vi } from 'vitest';
 import { Character } from './character.js';
-import { Person } from './person.js';
+
+vi.mock('./roll-dice.js', () => ({
+  rollDice: () => 10,
+}));
 
 describe('Character', () => {
-  it.todo(
-    'should create a character with a first name, last name, and role',
-    () => {},
-  );
+  let character: Character;
+  let characterDetails: { firstName: string; lastName: string; role: string };
 
-  it.todo('should allow you to increase the level', () => {});
+  beforeEach(() => {
+    characterDetails = {
+      firstName: 'Alice',
+      lastName: 'Smith',
+      role: 'Wizard',
+    };
+    character = new Character(
+      characterDetails.firstName,
+      characterDetails.lastName,
+      characterDetails.role,
+    );
+  });
 
-  it.todo('should update the last modified date when leveling up', () => {});
+  it('should create a character with a first name, last name, and role', () => {
+    expect(character).toEqual({
+      id: expect.stringContaining('person-'),
+      firstName: characterDetails.firstName,
+      lastName: characterDetails.lastName,
+      role: characterDetails.role,
+      level: 1,
+      createdAt: expect.any(Date),
+      lastModified: expect.any(Date),
+      charisma: 10,
+      constitution: 10,
+      dexterity: 10,
+      intelligence: 10,
+      strength: 10,
+      wisdom: 10,
+    });
+  });
+
+  it('should allow you to increase the level', () => {
+    character.levelUp();
+    expect(character.level).toBe(2);
+  });
+
+  it('should update the last modified date when leveling up', () => {
+    const oldLastModified = character.lastModified;
+    character.levelUp();
+    expect(character.lastModified).not.toBe(oldLastModified);
+  });
 });
